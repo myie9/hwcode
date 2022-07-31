c470a4fbbb69c6653e6a6aab7db9b0aeeac22585
gsrkuang <gsrkuang@163.com>
2022-07-31 18:43:20 +0800

    提交作业，IamPoem

---
+++ IamPoem/entry/src/main/java/com/iampoem/MyApplication.java
@@ -0,0 +1,10 @@
+package com.iampoem;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
